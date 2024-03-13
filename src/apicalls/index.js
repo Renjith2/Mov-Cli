4148065d1b8f0d2f063d52ed54c5bef2a17f05ad
Renjith2 <renjithkrishnan316@gmail.com>
2024-03-13 21:51:51 +0530

    use env variable for api base uri

--- src/apicalls/index.js
+++ src/apicalls/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
+    baseURL: process.env.REACT_APP_API_BASE_URL,
     headers:{
         credentials:"include", 
         method:"post",
